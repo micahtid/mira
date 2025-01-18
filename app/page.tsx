0fb3b1da1a527ea60742c79da02cb6fc9abc7d65
Micah Tidball <micahtid@gmail.com>
2025-01-18 14:17:55 +0700

    configured tailwind.config.ts, global.css & added Accordian.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Accordian from "@/components/Accordian"
+
 const Home = () => {
   return (
-    <div className="standard-container">
-      <div className="mb-[2000px]" />
+    <div className="default-container flex flex-col gap-4">
+      <h3 className="default-heading">This is Mira</h3>
+      <p className="default-text">We'll help you connect!</p>
+      <button className="default-button">
+        Hello There!
+      </button>
+      <Accordian title="What is Mira?" content="This is a test!" />
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,42 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        offBlack: "#222222",
-        minimalistGray: "#F5F5F5"
+        primary: {
+          50: "#f2efff",
+          100: "#dad0ff",
+          200: "#c1b0ff",
+          300: "#a790ff",
+          400: "#8265fe",
+          500: "#6d51fb",
+          600: "#5741c9",
+          700: "#413197",
+          800: "#2b2064",
+          900: "#161032",
+        },
+        secondary: {
+          50: "#eff5ff",
+          100: "#dfebff",
+          200: "#b3d0ff",
+          300: "#9fc3ff",
+          400: "#6ca3ff",
+          500: "#518afb",
+          600: "#416ec9",
+          700: "#315397",
+          800: "#203764",
+          900: "#101c32",
+        },
+        accent: {
+          50: "#fcf2ff",
+          100: "#f9e5ff",
+          200: "#f3ccff",
+          300: "#edb2ff",
+          400: "#e17eff",
+          500: "#c251fb",
+          600: "#9b41c9",
+          700: "#743197",
+          800: "#4d2064",
+          900: "#261032",
+        },
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif']
@@ -19,7 +53,7 @@ const config: Config = {
         'standard': '1500px',
       },
       spacing: {
-        'standard': '20px', 
+        'standard': '20px',
       }
     },
   },
